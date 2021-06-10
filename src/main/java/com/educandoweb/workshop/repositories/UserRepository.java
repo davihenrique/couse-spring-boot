6fb00d27125c5cb088f784103443a53abb3740f1
Davi Silva <davi.silva2597@gmail.com>
2021-06-10 15:45:47 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/educandoweb/workshop/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/workshop/repositories/UserRepository.java
@@ -2,10 +2,12 @@ package com.educandoweb.workshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.educandoweb.workshop.entities.User;
 
 
 
-public interface UserRepository  extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
+
